fa20dfac046f51aff009b0b8fc6f3a1e884d38a5
maroqijalil <jalilpro31@gmail.com>
2021-02-03 18:00:06 +0700

    init activity

---
+++ SiakadTK/app/src/main/java/app/siakad/siakadtk/AnnouncementActivity.kt
@@ -0,0 +1,11 @@
+package app.siakad.siakadtk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnnouncementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_announcement)
+    }
+}
---
+++ SiakadTK/app/src/main/java/app/siakad/siakadtk/ProfileSetActivity.kt
@@ -0,0 +1,11 @@
+package app.siakad.siakadtk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileSetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_set)
+    }
+}
