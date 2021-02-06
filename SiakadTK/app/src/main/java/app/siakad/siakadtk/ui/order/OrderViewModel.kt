99ca6595af49a9d3b7de93466dd518ca2cbca00d
finesaaa <indriatifiqey16@gmail.com>
2021-02-06 17:18:37 +0700

    minor

--- SiakadTK/app/src/main/java/app/siakad/siakadtk/ui/order/OrderViewModel.kt
+++ SiakadTK/app/src/main/java/app/siakad/siakadtk/ui/order/OrderViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class OrderViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Pemesanan"
     }
     val text: LiveData<String> = _text
 }
