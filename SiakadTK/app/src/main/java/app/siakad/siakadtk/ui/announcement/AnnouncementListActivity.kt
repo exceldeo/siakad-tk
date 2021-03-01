dc56ee1611cb8d21b6add5ef99386f1fc9e98e98
finesaaa <indriatifiqey16@gmail.com>
2021-03-01 19:00:13 +0700

    kotreq file

--- SiakadTK/app/src/main/java/app/siakad/siakadtk/ui/announcement/AnnouncementListActivity.kt
+++ SiakadTK/app/src/main/java/app/siakad/siakadtk/ui/announcement/AnnouncementListActivity.kt
@@ -2,12 +2,17 @@ package app.siakad.siakadtk.ui.announcement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.siakad.siakadtk.R
+import app.siakad.siakadtk.data.model.Announcement
 
 class AnnouncementListActivity : AppCompatActivity() {
 
     private lateinit var rvAnnouncement: RecyclerView
+    private var listAnnouncement: ArrayList<Announcement> = arrayListOf()
+    private lateinit var btnBack: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,21 @@ class AnnouncementListActivity : AppCompatActivity() {
 
     private fun setupItemView() {
         rvAnnouncement = findViewById(R.id.rv_announcement_daftar_pengumuman)
+        btnBack = findViewById(R.id.btn_announcement_back)
+        rvAnnouncement.setHasFixedSize(true)
+        listAnnouncement.addAll(AnnouncementsData.listData)
+        showAnnouncementRecyclerList()
     }
 
     private fun setupView() {
+        btnBack.setOnClickListener {
+            
+        }
+    }
 
+    private fun showAnnouncementRecyclerList() {
+        rvAnnouncement.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val announcementAdapter = AnnouncementInsideAdapter(listAnnouncement)
+        rvAnnouncement.adapter = announcementAdapter
     }
 }
