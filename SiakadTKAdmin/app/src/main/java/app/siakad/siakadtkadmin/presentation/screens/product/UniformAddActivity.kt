2478ce5869dda7e900b5eee1eeaad908447faedd
maroqijalil <jalilpro31@gmail.com>
2021-04-12 16:22:54 +0700

    add img picker

--- SiakadTKAdmin/app/src/main/java/app/siakad/siakadtkadmin/presentation/screens/product/UniformAddActivity.kt
+++ SiakadTKAdmin/app/src/main/java/app/siakad/siakadtkadmin/presentation/screens/product/UniformAddActivity.kt
@@ -1,15 +1,22 @@
 package app.siakad.siakadtkadmin.presentation.screens.product
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.RelativeLayout
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import app.siakad.siakadtkadmin.R
-import app.siakad.siakadtkadmin.presentation.screens.announcement.AnnouncementListFragment
 import app.siakad.siakadtkadmin.presentation.screens.product.dialog.UniformProductDialog
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
@@ -21,15 +28,20 @@ class UniformAddActivity : AppCompatActivity() {
 
     private lateinit var etName: EditText
     private lateinit var ddGender: TextInputLayout
+    private lateinit var ivPhotoPreview: ImageView
     private lateinit var btnAddPhoto: RelativeLayout
     private lateinit var btnAddData: RelativeLayout
     private lateinit var btnCancel: MaterialButton
     private lateinit var btnSave: MaterialButton
+    private lateinit var imgBtnAddPhoto: LinearLayout
     private lateinit var dialogUniform: UniformProductDialog
 
     companion object {
         const val GENDER_MAN = "Laki-laki"
         const val GENDER_WOMAN = "Perempuan"
+
+        const val PICK_PHOTO_REQUEST = 1000
+        const val PERMISSION_REQUEST = 1001
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +49,40 @@ class UniformAddActivity : AppCompatActivity() {
         setContentView(R.layout.activity_uniform_add)
 
         etName = findViewById(R.id.et_uniform_add_nama)
+        ivPhotoPreview = findViewById(R.id.iv_uniform_add_foto_preview)
+        imgBtnAddPhoto = findViewById(R.id.btn_uniform_add_add_foto)
 
         setupAppBar()
         setupButtons()
         setupDropDown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_PHOTO_REQUEST) {
+            val imageUri: Uri = data?.data!!
+            ivPhotoPreview.setImageURI(imageUri)
+            imgBtnAddPhoto.visibility = View.GONE
+        }
     }
 
     private fun setupAppBar() {
@@ -52,6 +94,21 @@ class UniformAddActivity : AppCompatActivity() {
 
     private fun setupButtons() {
         btnAddPhoto = findViewById(R.id.iv_uniform_add_foto)
+        btnAddPhoto.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    requestPermissions(permissions, PERMISSION_REQUEST);
+                } else {
+                    pickImageFromGallery();
+                }
+            } else {
+                pickImageFromGallery();
+            }
+        }
+
         btnCancel = findViewById(R.id.btn_uniform_add_batal)
         btnSave = findViewById(R.id.btn_uniform_add_simpan)
 
@@ -86,5 +143,11 @@ class UniformAddActivity : AppCompatActivity() {
 
             override fun onTextChanged(str: CharSequence?, start: Int, before: Int, count: Int) {}
         })
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_PHOTO_REQUEST)
     }
 }
