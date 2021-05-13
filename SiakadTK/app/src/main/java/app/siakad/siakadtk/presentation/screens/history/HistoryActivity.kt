a5bd65a982b0bfc9941393e3798ecf338a49af28
finesaaa <indriatifiqey16@gmail.com>
2021-05-13 18:55:10 +0700

    feat: add history

--- SiakadTK/app/src/main/java/app/siakad/siakadtk/presentation/screens/history/HistoryActivity.kt
+++ SiakadTK/app/src/main/java/app/siakad/siakadtk/presentation/screens/history/HistoryActivity.kt
@@ -5,9 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.siakad.siakadtk.presentation.screens.main.MainActivity
 import app.siakad.siakadtk.R
+import app.siakad.siakadtk.domain.utils.helpers.model.OrderStateModel
+import app.siakad.siakadtk.infrastructure.data.Pesanan
+import app.siakad.siakadtk.infrastructure.viewmodels.screens.order.OrderViewModel
+import app.siakad.siakadtk.infrastructure.viewmodels.utils.factory.ViewModelFactory
+import app.siakad.siakadtk.presentation.screens.history.adapter.OrderHistoryAdapter
+import app.siakad.siakadtk.presentation.screens.history.detail.HistoryDetailActivity
+import app.siakad.siakadtk.presentation.screens.order.adapter.OrderAdapter
+import app.siakad.siakadtk.presentation.screens.order.detail.OrderDetailActivity
 
 class HistoryActivity : AppCompatActivity() {
     private val pageTitle = "Riwayat Pembayaran"
@@ -15,16 +26,24 @@ class HistoryActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var rvHistory: RecyclerView
 
+    private lateinit var vmOrderList: OrderViewModel
+    private lateinit var rvHistoryAdapter: OrderHistoryAdapter
+    private lateinit var orderListObserver: Observer<ArrayList<Pesanan>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
         setupItemWiew()
         setupView()
+        setupObserver()
     }
 
     private fun setupItemWiew() {
         toolbar = findViewById(R.id.toolbar_main)
         rvHistory = findViewById(R.id.rv_history_order_list)
+
+        rvHistory.setHasFixedSize(true)
+        rvHistoryAdapter = OrderHistoryAdapter()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -44,11 +63,46 @@ class HistoryActivity : AppCompatActivity() {
 
     private fun setupView() {
         setupAppBar()
+
+        rvHistoryAdapter.setOnItemClickCallback(object: OrderHistoryAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Pesanan) {
+                val intent = Intent(this@HistoryActivity, HistoryDetailActivity::class.java)
+                intent.putExtra("pesanan_selesai", data);
+                startActivity(intent)
+            }
+        })
+
+        rvHistory.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@HistoryActivity)
+            adapter = rvHistoryAdapter
+        }
+
+        vmOrderList = ViewModelProvider(
+            this,
+            ViewModelFactory(this, this)
+        ).get(OrderViewModel::class.java)
     }
 
     private fun setupAppBar() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = pageTitle
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setupObserver() {
+        orderListObserver = Observer { list ->
+            if (list.size > 0) {
+                var doneList = arrayListOf<Pesanan>()
+                for (item in list)
+                {
+                    if(item.pesanan.statusPesan == OrderStateModel.ORDER_DONE.str)
+                        doneList.add(item)
+                }
+                rvHistoryAdapter.changeDataList(doneList)
+            }
+        }
+
+        vmOrderList.getOrderList().observe(this, orderListObserver)
     }
 }
