8e773cc9061aec06c9651bf68a9f0431c19b369c
maroqijalil <jalilpro31@gmail.com>
2021-05-14 01:26:24 +0700

    feat: improve dialog confirm

--- SiakadTKAdmin/app/src/main/java/app/siakad/siakadtkadmin/presentation/screens/order/detail/helper/OrderDetailHelper.kt
+++ SiakadTKAdmin/app/src/main/java/app/siakad/siakadtkadmin/presentation/screens/order/detail/helper/OrderDetailHelper.kt
@@ -3,4 +3,6 @@ package app.siakad.siakadtkadmin.presentation.screens.order.detail.helper
 interface OrderDetailHelper {
   fun checkTheItem(pos: Int)
   fun uncheckTheItem(pos: Int)
+
+  fun confirmOrderDate(start: String, end: String)
 }
